8bf93ec10282fa62f261e2c8ccbadd2a0e2555cc
wadhahjemai <Wadhah.Jemai@deutschebahn.com>
2023-12-11 23:36:47 +0100

    feat: implement password generation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/WadhahJemai/psgen
+
+go 1.21.3
---
+++ pkg/utils/env.go
@@ -0,0 +1,45 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Env interface {
+	int | string | bool
+}
+
+func GetEnv[T Env](key string, defaultValue string, required bool) T {
+	var val T
+
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		if required {
+			panic(fmt.Sprintf("env variable %s is required", key))
+		}
+		value = defaultValue
+	}
+
+	switch ptr := any(&val).(type) {
+	case *string:
+
+		*ptr = value
+	case *int:
+		target, err := strconv.Atoi(value)
+		if err != nil {
+			panic(fmt.Sprintf("env variable %s=%v can not be paresed to int", key, value))
+		}
+
+		*ptr = target
+	case *bool:
+		target, err := strconv.ParseBool(value)
+		if err != nil {
+			panic(fmt.Sprintf("env variable %s=%v can not be paresed to bool", key, value))
+		}
+
+		*ptr = target
+	}
+
+	return val
+}
